51ee2e6e2e3fd4fc5447d03b08a31f7ff90d62c2
Benjamin Kyrlach <bkyrlach@gmail.com>
2024-12-03 10:42:25 -0500

    Trying to model the halting problem.

--- Halting.lean
+++ Halting.lean
@@ -1,16 +1,5 @@
--- Declare a type `Prog` representing programs
-axiom Prog : Type
+inductive Program where
+  | T : Input -> Output -> Program
+  | NT : Input -> Program
 
--- Define a predicate `halts` that determines if a program halts
-axiom halts : Prog → Prop
 
--- Assume decidability of the halting predicate for any program
-axiom halt_dec : ∀ p : Prog, Decidable (halts p)
-
--- Assume the existence of a "diagonal" program
-axiom diagonal : Prog → Prog
-
--- Define the behavior of the diagonal program using an axiom
-axiom diagonalProg : ∀ p : Prog, halts (diagonal p) ↔ ¬ halts p
-
-theorem Halt_False : ∃ p, halts p -> ¬ halts p := by
