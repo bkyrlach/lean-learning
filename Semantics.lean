8b1d75a583463a11f8c6ebb8541f2a7419d40392
Nathan St Amour <nathan@thatdot.com>
2025-07-21 12:50:35 -0400

    Deterministic step proof. Feel free to delete parts of it.

--- Semantics.lean
+++ Semantics.lean
@@ -64,25 +64,27 @@ def stepProg (e : exp): exp :=
     Plus (stepProg e1) e2
 
 
+inductive step_value : exp -> Prop where
+| vConst : forall n, step_value (C n)
+
+
 inductive step : exp -> exp -> Prop where
-| stepConst1 : forall n, step (C n) (C n)
 
 | stepPlusConst : forall n m, step (Plus (C n) (C m)) (C (n + m))
 
-| stepPlusConstLeft: forall n e resultExp,
+| stepPlusConstLeft: forall e resultExp eValue,
     step e resultExp ->
-    step (Plus (C n) e) (Plus (C n) resultExp)
+    step_value eValue ->
+    step (Plus eValue e) (Plus eValue resultExp)
 
 | stepPlusLeftStep : forall e1 e2 resultExp,
     step e1 resultExp ->
     step (Plus e1 e2) (Plus resultExp e2)
 open step
 
-infix:50 " ==> " => step
+/- infix:50 " ==> " => step
 
-example : (C 0) ==> (C 0) := by apply stepConst1
-
-example : (Plus (C 0) (C 1)) ==> (C 1) := by
+example : step (Plus (C 0) (C 1)) (C 1) := by
   apply stepPlusConst
 
 example : (Plus (Plus (C 0) (C 1)) (Plus (C 2) (C 3))) ==>
@@ -99,3 +101,112 @@ example : (Plus (C 1) (Plus (C 2) (C 3))) ==>
 example : ¬ (Plus (Plus (C 1) (C 2)) (Plus (C 2) (C 3))) ==>
   (Plus (Plus (C 1) (C 2)) (C 5))
  := by sorry
+ -/
+
+-- Lets formalize some stuff about relations.
+
+-- even
+   --- exists n = 2 * m
+
+-- even 6
+--  - sub2 : even 4
+--  - sub4 : even 2
+
+
+def relation (X : Type) := X -> X -> Prop
+def value (X : Type) := X -> Prop
+
+def deterministic {X : Type} (R : relation X) :=
+  forall x y1 y2, R x y1 -> R x y2 -> y1 = y2
+
+def progress {X : Type} (R : relation X) (valueP : value X) :=
+  forall x: X, exists y, R x y ∨ valueP x
+
+def normal_formal {X : Type} (R : relation X) (x : X) :=
+  ¬ exists y, R x y
+
+theorem step_deterministic : deterministic step := by
+  unfold deterministic
+  intros x y1 y2
+  intros H1 H2
+  revert y2
+  induction H1 with
+  | stepPlusConst n m =>
+    intros y2
+    intros H2
+    have y2Plus: y2 = C (n + m) := by
+      cases H2 with
+      | stepPlusConst n m => simp
+      | stepPlusConstLeft e resultExp eValue H3 H4 =>
+        cases H3
+      | stepPlusLeftStep e1 e2 resultExp a =>
+        cases a
+    rw [y2Plus]
+  | stepPlusConstLeft e resultExp eValue H3 H4 ih =>
+    intros y2
+    intros H2
+    cases H2 with
+    | stepPlusConst n m => cases H3
+    | stepPlusConstLeft e_ resultExp_ eValue_ H3_ H4_ =>
+      simp
+      apply ih
+      simp [*]
+    | stepPlusLeftStep e1 e2 resultExp a =>
+        cases H4
+        cases a
+  | stepPlusLeftStep e1 e2 resultExp a ih =>
+    intros y2
+    intros H2
+    cases H2 with
+    | stepPlusConst n m => cases a
+    | stepPlusConstLeft e_ resultExp_ eValue_ H3_ H4_ =>
+      cases H4_
+      cases a
+    | stepPlusLeftStep e1_ e2_ resultExp_ a_ =>
+      simp
+      apply ih
+      simp [*]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def deterministic {X : Type} (R : relation X) :=
+  forall x y1 y2 : X, R x y1 -> R x y2 -> y1 = y2
+
+theorem step_deterministic : deterministic step := by
+  unfold deterministic
+  intros x y1 y2 H1 H2
+  revert y2
+  induction H1 <;> intros
+  have H2: C
